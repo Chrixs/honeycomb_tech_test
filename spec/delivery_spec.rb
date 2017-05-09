a1a00ef2122f97d74f16e6e9cad20ed40db6015a
Chris <chrixs99@hotmail.com>
2017-05-09 20:45:21 +0100

    delivery class added and tested

---
+++ spec/delivery_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'delivery'
+
+describe Delivery do
+
+    name = "express"
+    price = 20
+    subject { described_class.new(name, price) }
+
+    it "should have a name attribute" do
+      expect(subject.name).to eq("express")
+    end
+
+    it "should have a price attribute" do
+      expect(subject.price).to eq(20.00)
+    end
+
+    it "price attribute should be a float" do
+      expect(subject.price).to be_a(Float)
+    end
+
+end
