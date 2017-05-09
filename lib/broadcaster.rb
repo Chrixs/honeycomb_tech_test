9d04c19cd8268fe47bdf533a023a815be8c4590f
Chris <chrixs99@hotmail.com>
2017-05-09 19:33:14 +0100

    created and tested broadcaster class

---
+++ lib/broadcaster.rb
@@ -0,0 +1,7 @@
+class Broadcaster
+  attr_accessor :name
+
+  def initialize(name)
+    self.name = name
+  end
+end
---
+++ spec/broadcaster_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'broadcaster'
+
+describe Broadcaster do
+
+  subject { described_class.new("Viacom") }
+
+  it "should have a name attribute" do
+    expect(subject.name).to eq("Viacom")
+  end
+
+end
