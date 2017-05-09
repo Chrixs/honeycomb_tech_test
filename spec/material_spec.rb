08cc60a48826f3e516d014c83ed9b4069ce31c12
Chris <chrixs99@hotmail.com>
2017-05-09 19:44:23 +0100

    material class created and tested

---
+++ spec/material_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'material'
+
+describe Material do
+
+  id = "WNP/SWCL001/010"
+  subject { described_class.new(id) }
+
+  it "should have an id attribute" do
+    expect(subject.id).to eq("WNP/SWCL001/010")
+  end
+end
